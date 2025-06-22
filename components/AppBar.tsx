99fe008190b2f7f61a2ab98dd90e75efda4f602a
nszuev <nszuev@avito.ru>
2025-06-22 15:44:26 +0300

    Fixed problem with server side rendering

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -2,13 +2,20 @@ import { FC } from 'react'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+
+
+const WalletConnectButton = dynamic(
+    () => import("@solana/wallet-adapter-react-ui").then(mod => mod.WalletMultiButton),
+    { ssr: false }
+);
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
             <Image src="/solanaLogo.png" height={30} width={200} />
             <span>Wallet-Adapter Example</span>
-            <WalletMultiButton></WalletMultiButton>
+            <WalletConnectButton ></WalletConnectButton>
         </div>
     )
 }
